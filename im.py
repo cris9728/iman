946fa85d70878fcfdbb1ec0c15d7e9891f2aacbf
cris9728 <47966231+cris9728@users.noreply.github.com>
2019-02-25 14:31:18 +0530

    Add files via upload

---
+++ im.py
@@ -0,0 +1,20 @@
+str3="""welcome 
+to
+bppimt"""
+print(str3)
+num = int(input("enter the number"))
+if num%2 == 0:  
+    print("Number is even")  
+else:
+    print("number is odd")
+a=int(input("enter a"))
+b=int(input("enter b"))
+c=int(input("enter c"))
+if a>b and a>c:
+     print("a is leargest")
+elif b>a and b>c:
+     print("b is leargest")
+else:
+     print("c is leargest")
+
+
