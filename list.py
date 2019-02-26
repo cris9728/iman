b381dfc9f74e9dfed72df25f0c0a20f65e21daff
iman ghosh <imanghosh.6666@gmail.com>
2019-02-26 12:01:00 +0530

    initial

---
+++ list.py
@@ -0,0 +1,24 @@
+l  = [1, "hi", "python", 2]  
+print (l[3:]);  
+print (l[0:2]);  
+print (l);  
+print (l + l);  
+print (l * 3);   
+t  = ("hi", "python", 2)  
+print (t[1:]);  
+print (t[0:1]);  
+print (t);  
+print (t + t);  
+print (t * 3);   
+print (type(t))  
+t[2] = "hi";  
+d = {1:'Jimmy', 2:'Alex', 3:'john', 4:'mike'};   
+print("1st name is "+d[1]);  
+print("2nd name is "+ d[4]);  
+print (d);  
+print (d.keys());  
+print (d.values()); 
+str3="""welcome 
+to
+bppimt"""
+print str3
